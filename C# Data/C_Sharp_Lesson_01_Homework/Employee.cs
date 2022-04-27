b51b2b64b9352f70b14661d911d82a7daa8d1b25
MarianPopov <info.gmp92@gmail.com>
2022-04-27 12:02:09 +0300

    C# Data

---
+++ C# Data/C_Sharp_Lesson_01_Homework/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lesson_01_Homework
+{
+    public class Employee
+    {
+        public string name { set { this.name = name; } get { return this.name; } }
+        public double rate { set { this.rate = rate; } get { return this.rate; } }
+        public DateTime employmentDate { set { this.employmentDate = employmentDate; } get { return this.employmentDate; } }
+
+        public Employee(string name, double rate, DateTime employmentDate)
+        {
+            this.name = name;
+            this.rate = rate;
+            this.employmentDate = employmentDate;
+        }
+    }
+}
---
+++ C# Data/C_Sharp_Lesson_01_Homework/IEmployeeOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lesson_01_Homework
+{
+    public interface IEmployeeOperations
+    {
+       double SalaryCalculator(int numberOfDays, double rate);
+       int Experience(DateTime dateOfEmploymnet);
+    }
+}
