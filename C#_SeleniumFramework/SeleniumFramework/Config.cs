b12b2f3e5edcd8b8f448410c2e8da25a4ce6a1c6
MarianPopov <info.gmp92@gmail.com>
2022-05-31 14:49:08 +0300

    implicitWaitTime set 30 seconds

--- C#_SeleniumFramework/SeleniumFramework/Config.cs
+++ C#_SeleniumFramework/SeleniumFramework/Config.cs
@@ -4,7 +4,7 @@
     {
         public static string baseUrl = "https://projectplanappweb-stage.azurewebsites.net/";
         public static string browser = "chrome";
-        public static int implicitWaitTime = 0;
+        public static int implicitWaitTime = 30;
         public static int ShortTimeout = 5;
         public static int MediumTimeout = 15;
         public static int LongTimeout = 30;
